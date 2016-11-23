b22cf623b8eed3ac26e2250c302e31f171d12111
Alan Tai <alan.tai@riverbed.com>
2016-11-23 12:51:33 -0800

    add angular 1.5

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('myApp', ['periscope'])
 .controller('Ctrl1', ['$scope', function($scope) {
-  $scope.framework = 'Angular v1.4';
+  $scope.framework = 'Angular v1.5';
 }]);
